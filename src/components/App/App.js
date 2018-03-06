eefe58a67a606b6601af9d87faff8cc1e3a19600
Zack Piper <zack@chronosinteractive.com>
2018-03-05 18:40:12 -0700

    add prettier

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,20 @@
 import React, { Component } from 'react';
+import CarList from '../CarList/CarList';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <header>Hello from the header</header>
+
+        <main>
+          Hellow from main
+          <CarList />
+        </main>
+
+        <footer>Hello from footer</footer>
       </div>
     );
   }
